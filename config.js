0bdddaf8b44d67db2be49a816d6b8f017af2bbe4
Thienlovehien <86721296+Thienlovehien@users.noreply.github.com>
2021-07-01 19:57:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
     introTitle: 'Hiền à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '=))))',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích mày <33',
+    introDesc: `Hỏi mày câu này`,
+    btnIntro: 'OK =)))))',
+    title: 'Yêu mày nhiều lắm <3😙',
+    desc: 'Mỗi ngày tao chỉ cần được mày nói "yêu" thôii ',
+    btnYes: 'Nhất trí :vvv',
     btnNo: 'Không, nằm mơ à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao m lại yêu t <3',
     btnReply: 'Gửi cho Thiện <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'T biết mà 🥰. Yêu m nhiều nhiều 😘😘',
-    messDesc: 'Không nhắn tin cho m nhiều nên t làm cái này vậy .:vvv.',
+    mess: 'T biết mà 🥰. Yêu Hiền x300000000000 😘😘',
+    messDesc: 'Tao còn bám mày dàii ',
     btnAccept: '=))))) <3',
     messLink: 'http://https://www.facebook.com/profile.php?id=100005036799960' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
